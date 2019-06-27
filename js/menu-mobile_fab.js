f80e17d8ba3c4dc18a6c93d51807006a9bace7e5
xempoweredx <chadgray1@gmail.com>
2019-06-27 15:25:01 -0500

    Update menu-mobile_fab.js

--- js/menu-mobile_fab.js
+++ js/menu-mobile_fab.js
@@ -5,8 +5,36 @@
     	$('.offcanvas-collapse').toggleClass('open');
     	$('.fab-toggler-wrap').toggleClass('menu-open').toggleClass('menu-closed');
 
-    	//$('.toggler-icon .menu-icon').toggleClass('d-none');
-
   })
+
+	var position = $(window).scrollTop(); 
+
+	// should start at 0
+
+	$(window).scroll(function() {
+	    var scroll = $(window).scrollTop();
+	    if(scroll > position) {
+	        console.log('scrollDown');
+	        $('.icon-placeholder').animate({width: '27px'}, 200);
+	        $('.scroll-menu-text').animate({right: '16px', opacity: '0'}, 200); 
+	    } else {
+	         console.log('scrollUp');
+	         $('.icon-placeholder').animate({width: '92px'}, 200);
+	         $('.scroll-menu-text').animate({right: '46px', opacity: '1'}, 200); 
+	    }
+	    position = scroll;
+	});
+
+	// To prevent "Added non-passive event listener to a scroll-blocking 'touchstart ..." error in Chrome for .click functions
+ 	$.event.special.touchstart = {
+  		setup: function( _, ns, handle ){
+    		if ( ns.includes("noPreventDefault") ) {
+      			this.addEventListener("touchstart", handle, { passive: false });
+    		} 
+    		else {
+      			this.addEventListener("touchstart", handle, { passive: true });
+    		}
+  		}
+	};
 
 })( jQuery );
