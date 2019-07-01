bfbc2cdf53d2c50bc4cda5936e693966e633be1d
xempoweredx <chadgray1@gmail.com>
2019-06-30 22:33:14 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -43,23 +43,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 </div><!-- wrapper end -->
 
-<!-- Modal -->
-<div class="modal fade" id="bugModal" tabindex="-1" role="dialog" aria-labelledby="bugModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="bugModalLabel">Bug Track/Error Report</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      	<?php echo do_shortcode('[wpforms id="4209"]'); ?>
-      </div>
-    </div>
-  </div>
-</div>
-
 </div><!-- #page we need this extra closing tag here -->
 
 <?php wp_footer(); ?>
